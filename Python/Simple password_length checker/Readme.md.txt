fd4c824ffd18a24f58ab77c1d2d5df3bfbb5f29d
mihaibaban96 <mbaban1996@gmail.com>
2022-12-11 12:53:42 +0200

    Add "Simple length password checker"

---
+++ Python/Simple password_length checker/Readme.md.txt
@@ -0,0 +1,8 @@
+# Simple password length checker
+This is a simple checker in python.
+Purpose of this is to check the length of an inputed password.
+
+
+Educational purpose:
+	--->Learn input function in python
+	-->Learn to work with strings in python
---
+++ Python/Simple password_length checker/password_length_checker.py
@@ -0,0 +1,12 @@
+########################################################### SIMPLE PASSWORD LENGTH CHECKER IN PYTHON ############################################################
+########################################################### SIMPLE PASSWORD LENGTH CHECKER IN PYTHON ############################################################
+########################################################### SIMPLE PASSWORD LENGTH CHECKER IN PYTHON ############################################################
+
+
+username = input("Provide your username\n")
+password = input("Provide your password\n")
+
+password_length = len(password)
+hidden_password = '*' * password_length
+
+print(f"{username}, your password, {hidden_password}, is {password_length} letters long!")
